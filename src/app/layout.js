4a1d4ba171bc33e01afb20eedad2a178ae6fbdb1
刘传 <769601722@qq.com>
2024-01-12 14:19:32 +0800

    谷歌分析

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,14 +37,16 @@ export default function RootLayout({ children }) {
         src="https://www.googletagmanager.com/gtag/js?id=G-8KHW9QWHF2"
       ></script>
       <script
-        dangerouslySetInnerHTML={`
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
 
-        gtag("config", "G-8KHW9QWHF2");`}
+            gtag("config", "G-8KHW9QWHF2");`,
+        }}
       ></script>
     </html>
   );
